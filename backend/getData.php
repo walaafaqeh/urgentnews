478738d5b49807e34a365d643f0c7664f9b42bb1
Mac User <macuser@Macs-MacBook-Pro.local>
2023-05-19 00:42:12 +0300

    changes

---
+++ backend/getData.php
@@ -0,0 +1,44 @@
+<?php
+$hostName = "localhost";
+$userName = "walaa";
+$password = "1234";
+$databaseName = "NEWS";
+ $conn = new mysqli($hostName, $userName, $password, $databaseName);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$db= $conn;
+$tableName="Urgents";
+$columns= ['Title','Content'];
+$fetchData = getUrgentNews($db, $tableName, $columns);
+
+function getUrgentNews($db, $tableName, $columns){
+ if(empty($db)){
+  $msg= "Database connection error";
+ }elseif (empty($columns) || !is_array($columns)) {
+  $msg="columns Name must be defined in an indexed array";
+ }elseif(empty($tableName)){
+   $msg= "Table Name is empty";
+}else{
+
+$columnName = implode(", ", $columns);
+$query = "SELECT ".$columnName." FROM $tableName"." ORDER BY id DESC";
+$result = $db->query($query);
+
+if($result== true){ 
+ if ($result->num_rows > 0) {
+    $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $msg= $row;
+ } else {
+    $msg= "No Data Found"; 
+ }
+}else{
+  $msg= mysqli_error($db);
+}
+}
+return $msg;
+}
+?>
